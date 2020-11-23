5e1fb584d8c57c9302ec61915f74df34d1d4640f
Madziks <majta.magdalena@gmail.com>
2020-11-23 16:47:48 +0100

    first little success xD in db

--- Dziennik.py
+++ Dziennik.py
@@ -37,7 +37,7 @@ class LectureGroup(BazaModel):
     lecturegroup = relationship("Lecture", back_populates="group")
 
     group_id = Column(Integer, ForeignKey('Groups.id'))
-    lecturegroup = relationship("Group", back_populates="lecture")"""       aż do tąd xD
+    lecturegroup = relationship("Group", back_populates="lecture")"""      # az dotad xD
 
 
 class Student(BazaModel):
@@ -50,6 +50,23 @@ class Student(BazaModel):
     #group_id = Column(Integer, ForeignKey('Groups.id'))
     #group = relationship("Group", back_populates="student")
 
+class GradeCategory(BazaModel):
+    __tablename__ = 'GradesCategories'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    grade = relationship('Grade', back_populates='grade_category')
+
+class Grade(BazaModel):
+    __tablename__ = 'Grades'
+    id = Column(Integer, primary_key = True)
+    value = Column(Integer, nullable=False)
+    category_id = Column(Integer, ForeignKey('GradesCategories.id'))
+    grade_category = relationship('GradeCategory', back_populates='grade')
+
+    #student_id = Column(Integer, nullable = False)
+    #lecture_id = Column(Integer, nullable = False)
+
+
 BazaModel.metadata.create_all(baza)
 
 BDSesja = sessionmaker(bind=baza)
@@ -57,7 +74,27 @@ sesja = BDSesja()
 
 if not sesja.query(Student).count():
     sesja.add(Student(FirstName='Kryha', LastName='Szura'))
-    sesja.add(Student(FirstName = 'Madzionator', LastName = 'Madzik'))
+    sesja.add(Student(FirstName='Madzionator', LastName='Madzik'))
 
-for student in sesja.query(Student).all():
-    print(student.id, student.FirstName, student.LastName)
+if not sesja.query(GradeCategory).count():
+    sesja.add(GradeCategory(name='Kolos1'))
+    sesja.add(GradeCategory(name='Kolos2'))
+    sesja.add(GradeCategory(name='Test'))
+
+if not sesja.query(Grade).count():
+    sesja.add(Grade(value = 4, category_id = 3))
+    sesja.add(Grade(value = 3, category_id = 2))
+    sesja.add(Grade(value = 5, category_id = 2))
+
+#for student in sesja.query(Student).all():
+#    print(student.id, student.FirstName, student.LastName)
+
+#for dane in sesja.query(Grade.value, GradeCategory.name).join(GradeCategory, GradeCategory.id == Grade.category_id).all():
+#    print (dane)
+
+for kat in sesja.query(GradeCategory).all():
+    print (kat)
+
+for grade in sesja.query(Grade).all():
+    print (grade)
+    
