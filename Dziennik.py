24b80230f60c9bace3a5c2ba051c2af5185b51c0
Madziks <majta.magdalena@gmail.com>
2020-11-25 01:06:40 +0100

    😴 🙄???

--- Dziennik.py
+++ Dziennik.py
@@ -1,5 +1,6 @@
-from Baza import sesja, Subject, Group, SubjectGroup, Student, Grade, GradeCategory
+'''from Baza import sesja, Subject, Group, SubjectGroup, Student, Grade, GradeCategory
 import tkinter as tk
+#from subject_add_edit import Group_window
 
 def unpack(subject_list):
     finally_list = []
@@ -10,9 +11,6 @@ def unpack(subject_list):
 def unpack_choice(choice):
     finally_choice = choice[0]
     return finally_choice
-
-def openB():
-    print ("open")
 
 def editB():
     print("edit")
@@ -25,6 +23,7 @@ def addB():
 
 class Application:
     def __init__(self):
+
         self.root = tk.Tk()
         self.root.geometry("600x600")
         self.root.title("Dziennik")
@@ -40,10 +39,10 @@ class Application:
 
         def SubjectSelect(event):
             choice = unpack_choice(self.subject_list.curselection())
-            print(subjects[choice])
+            print(subjects[choice]) #tymczasowe
         self.subject_list.bind('<<ListboxSelect>>', SubjectSelect)
 
-        open_button = tk.Button(self.root, text="otwórz", width=20, command = openB)
+        open_button = tk.Button(self.root, text="otwórz", width=20, command = self.switch_frame(Group_window))
         open_button.pack(anchor = 'w')
 
         edit_button = tk.Button(self.root, text="edytuj", width=20, command = editB)
@@ -57,4 +56,47 @@ class Application:
 
         self.root.mainloop()
 
-app = Application()
+    def switch_frame(self, frame_class):
+        new_frame = frame_class(self)
+        if self._frame is not None:
+            self._frame.destroy()
+        self._frame = new_frame
+        self._frame.pack()
+
+class Group_window(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        print("cokolwiek")
+
+
+app = Application()'''
+
+import tkinter as tk
+from subject_add_edit import PageOne, PageTwo
+
+class Application(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self._frame = None
+        self.switch_frame(StartPage)
+
+    def switch_frame(self, frame_class):
+        """Destroys current frame and replaces it with a new one."""
+        new_frame = frame_class(self)
+        if self._frame is not None:
+            self._frame.destroy()
+        self._frame = new_frame
+        self._frame.pack()
+
+class StartPage(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        tk.Label(self, text="This is the start page").pack(side="top", fill="x", pady=10)
+        tk.Button(self, text="Open page one",
+                  command=lambda: master.switch_frame(PageOne)).pack()
+        tk.Button(self, text="Open page two",
+                  command=lambda: master.switch_frame(PageTwo)).pack()
+
+if __name__ == "__main__":
+    app = Application()
+    app.mainloop()
---
+++ subject_add_edit.py
@@ -0,0 +1,16 @@
+#from Baza import sesja, Subject, Group, SubjectGroup, Student, Grade, GradeCategory
+import tkinter as tk
+
+class PageOne(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        tk.Label(self, text="This is page one").pack(side="top", fill="x", pady=10)
+        tk.Button(self, text="Return to start page",
+                  command=lambda: master.switch_frame(StartPage)).pack()
+
+class PageTwo(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        tk.Label(self, text="This is page two").pack(side="top", fill="x", pady=10)
+        tk.Button(self, text="Return to start page",
+                  command=lambda: master.switch_frame(StartPage)).pack()
