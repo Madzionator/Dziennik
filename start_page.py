a5c295eeb2a9b2bcab792c237a21f66d991cd3a3
Madziks <majta.magdalena@gmail.com>
2020-11-26 19:29:03 +0100

    subject_delete added 👍

--- start_page.py
+++ start_page.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox as msb
 from Baza import sesja, Subject
 from subject_add_edit import Subject_Add, Subject_Edit
 
@@ -21,8 +22,10 @@ class StartPage(tk.Frame):
         self.load_subject()
         self.subject_list.pack(anchor = 'w')
 
+        self.choice = 0
         def SubjectSelect(event):
-            choice = unpack_choice(self.subject_list.curselection())
+            self.choice = self.get_choice()  # by name
+
         self.subject_list.bind('<<ListboxSelect>>', SubjectSelect)
 
         '''open_button = tk.Button(self, text="otwórz", width=20)
@@ -33,13 +36,28 @@ class StartPage(tk.Frame):
 
         tk.Button(self, text="Dodaj nowy", command=lambda: master.navigate_to(Subject_Add)).pack()
         tk.Button(self, text="Edytuj", command=lambda: master.navigate_to(Subject_Edit)).pack()
+        tk.Button(self, text="Usuń", command=self.delete_subject).pack()
 
     def load_subject(self):
         self.subject_list.delete(0, tk.END)
         subjects = unpack(sesja.query(Subject.name).order_by(Subject.name).all())
         for i in range(len(subjects)):
             self.subject_list.insert(i, subjects[i])
-        
+
+    def get_choice(self):
+        choice = unpack_choice(self.subject_list.curselection())
+        subjects = unpack(sesja.query(Subject.name).order_by(Subject.name).all())
+        str_choice = subjects[choice]
+        return str_choice
 
     def on_back(self):
         self.load_subject()
+
+    def delete_subject(self):
+        if not self.choice:
+            msb.showinfo(None, "Nie wybrano przedmiotu do usunięcia.")
+            return
+        if msb.askokcancel(None, ("Na pewno chcesz usunąć?") ):
+            sesja.query(Subject).filter(Subject.name==self.choice).delete()
+            self.load_subject()
+            self.choice = 0
--- subject_add_edit.py
+++ subject_add_edit.py
@@ -26,7 +26,7 @@ class Subject_Add(tk.Frame):
             group.pack(anchor = 'w')
 
         tk.Button(self, text="Zapisz", command = self.save).pack()
-        tk.Button(self, text="Return to start page", command=lambda: master.go_back()).pack()
+        tk.Button(self, text="Wróć", command=lambda: master.go_back()).pack()
 
     def get_selected_groups(self):
         checked_groups = []
@@ -56,6 +56,51 @@ class Subject_Add(tk.Frame):
 class Subject_Edit(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
-        tk.Label(self, text="Edit").pack(side="top", fill="x", pady=10)
-        tk.Button(self, text="Return to start page",
-                  command=lambda: master.go_back()).pack()
+        tk.Label(self, text="Podaj nową nazwę przedmiotu: ").pack(side="top", fill="x", pady = 4, anchor = 'w')
+        self.name_entry = tk.Entry(self)
+        self.name_entry.pack(anchor = 'w')
+
+        '''
+        self.all_groups = []
+        self.groups_checkbox = []
+        self.groups_checkbox_state = []
+        self.master = master
+
+        tk.Label(self, text="Wybierz przynależne grupy: ").pack(side="top", fill="x", pady = 4, anchor = 'w')
+
+        for group in sesja.query(Group).all():
+            var = tk.IntVar()
+            self.all_groups.append(group)
+            self.groups_checkbox.append(tk.Checkbutton(self, text=group.name, variable=var))
+            self.groups_checkbox_state.append(var);
+            
+        for group in self.groups_checkbox:
+            group.pack(anchor = 'w')'''
+
+        tk.Button(self, text="Zapisz", command = self.save).pack()
+        tk.Button(self, text="Wróć", command=lambda: master.go_back()).pack()
+'''
+    def get_selected_groups(self):
+        checked_groups = []
+        for i in range(len(self.all_groups)):
+            if self.groups_checkbox_state[i].get():
+                checked_groups.append(self.all_groups[i])
+        return checked_groups
+
+    def save(self):
+        name_str = self.name_entry.get()
+        if len(name_str)==0 :
+            msb.showwarning("Błąd", "Nie wprowadzono nazwy przedmiotu.")
+            return
+        elif len(name_str)>100:
+            msb.showwarning("Błąd", "Wprowadzona nazwa jest za długa.")
+            return
+        exists = sesja.query(Subject.name).filter_by(name = name_str).scalar() # None
+        if exists:
+            msb.showwarning("Błąd", "Podana nazwa jest już zajęta.")
+        else:
+            sesja.add(Subject(name = name_str))
+            id = sesja.query(Subject.id).filter_by(name = name_str).one()
+            for x in self.get_selected_groups():
+                sesja.add(SubjectGroup(subject_id = id[0], group_id = x.id))
+            self.master.go_back()'''
