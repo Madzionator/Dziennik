e28e60354f478d7825e6a0684becbb47a041db0e
Madziks <majta.magdalena@gmail.com>
2020-12-12 22:25:46 +0100

    Grade_edit (all parameters)

--- Dziennik.py
+++ Dziennik.py
@@ -3,26 +3,27 @@ from inspect import signature
 from start_page import StartPage
 
 class frame_data:
-    def __init__(self, frame, arg, arg2):
+    def __init__(self, frame, arg, arg2, arg3):
         self.frame = frame
         self.arg = arg
         self.arg2 = arg2
+        self.arg3 = arg3
 
 class Application(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         self.title('Dziennik')
-        self.geometry('1400x620') #X Y
+        self.geometry('1400x620')
         self.minsize(width = 550, height = 260)
         self.frame_stack = []
         self.current_frame = None
         self.navigate_to(StartPage)
 
-    def navigate_to(self, frame_class, arg = None, arg2 = None):
+    def navigate_to(self, frame_class, arg = None, arg2 = None, arg3 = None):
         if self.current_frame is not None:
             self.current_frame.destroy()
 
-        new_frame = frame_data(frame_class, arg, arg2)
+        new_frame = frame_data(frame_class, arg, arg2, arg3)
         self.frame_stack.append(new_frame)
         self.show_frame(new_frame)
 
@@ -41,6 +42,8 @@ class Application(tk.Tk):
             self.current_frame = frame_data.frame(self, frame_data.arg)
         elif frame_arg_count == 3:
             self.current_frame = frame_data.frame(self, frame_data.arg, frame_data.arg2)
+        elif frame_arg_count == 4:
+            self.current_frame = frame_data.frame(self, frame_data.arg, frame_data.arg2, frame_data.arg3)
         self.current_frame.pack()
 
 if __name__ == "__main__":
--- grade_manager_page.py
+++ grade_manager_page.py
@@ -1,6 +1,6 @@
 from Baza import sesja, Student, Subject, Grade, GradeCategory
+from grade_add_edit import Grade_Add, Grade_Edit
 import tkinter as tk
-import tkinter.ttk as ttk
 from tkinter import*
 from tkinter.simpledialog import askfloat
 from tkinter import messagebox as msb
@@ -64,74 +64,8 @@ class Grade_Manager(tk.Frame):
             self.grade_choice = 0
             sesja.commit()
 
-    def try_edit(self):
+    def try_edit(self): # to do !!!
         if self.grade_choice == 0:
             msb.showwarning("Błąd", "Nie wybrano oceny.")
             return
-        new_grade_value = askfloat(None, "Podaj nową wartość oceny")
-        if new_grade_value == None:
-            return
-        if new_grade_value > 5 or new_grade_value < 2 or not (new_grade_value*2).is_integer():
-            msb.showwarning("Błąd", "Wprowadzona wartość oceny jest nieprawidłowa.")
-            return
-        sesja.query(Grade).filter(Grade.id == self.grade_choice.id).update({Grade.value: new_grade_value})
-        sesja.commit()
-        self.load_grade()
-
-class Grade_Add(tk.Frame):
-    def __init__(self, master, student, subject):
-        tk.Frame.__init__(self, master)
-        tk.Label(self, text="Wybierz kategorię", font=("Calibri", 12)).grid(row = 0, columnspan = 2, sticky=N+E+S+W)
-        self.student = student
-        self.subject = subject
-        self.master = master
-
-        self.cb_value = tk.StringVar()     
-        self.combobox_grade_categories = ttk.Combobox(self, textvariable = self.cb_value, state="readonly", font=("Calibri", 12))
-        self.combobox_grade_categories.grid(row = 1, columnspan = 2, sticky=N+E+S+W)
-        grade_categories_obj = []
-        grade_categories = []
-        for category in sesja.query(GradeCategory).all():
-            grade_categories_obj.append(category)
-            grade_categories.append(category.name)
-        self.combobox_grade_categories['values'] = grade_categories
-        self.combobox_grade_categories.current(0)
-        self.category_choice = 1
-        self.combobox_grade_categories.bind("<<ComboboxSelected>>", self.select_category)
-
-        tk.Label(self, text="Podaj ocenę", font=("Calibri", 12)).grid(row = 2, columnspan = 2, sticky=N+E+S+W)
-        self.value_entry = tk.Entry(self, font=("Calibri", 12))
-        self.value_entry.grid(row = 3, columnspan = 2, sticky=N+E+S+W)
-
-        tk.Button(self, text="Zapisz", command=self.save, font=("Calibri", 10)).grid(row = 4, column = 1, sticky=N+E+S+W, pady=3)
-        tk.Button(self, text="Wróć", command=lambda: master.go_back(), font=("Calibri", 10)).grid(row = 4, column = 0, sticky=N+E+S+W, pady=3)
-
-        for i in range(0, 2):
-            self.grid_columnconfigure(i, weight = 2, uniform=True)
-    
-    def select_category(self, event):
-        str_choice = self.cb_value.get()
-        int_choice = sesja.query(GradeCategory.id).filter(GradeCategory.name == str_choice).one()
-        self.category_choice = int_choice[0]
-
-    def save(self):
-
-        value_str = self.value_entry.get()
-        if len(value_str) == 0:
-            msb.showwarning("Błąd", "Nie podano wartości oceny.")
-            return
-
-        try:
-            value_float = float(value_str)
-        except ValueError:
-            msb.showwarning("Błąd", "Wprowadzona wartość oceny jest nieprawidłowa.")
-            return
-
-        if value_float > 5 or value_float < 2 or not (value_float*2).is_integer():
-            msb.showwarning("Błąd", "Wprowadzona wartość oceny jest nieprawidłowa.")
-            return
-
-        sesja.add(Grade(value = value_float, grade_category_id = self.category_choice, student_id = self.student.id, subject_id = self.subject.id))
-        sesja.commit()
-        self.master.go_back()
-        
+        self.master.navigate_to(Grade_Edit, self.subject, self.student, self.grade_choice)
